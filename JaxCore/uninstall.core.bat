695cc05eb1de64f6601367d01da8f8d14582fe03
dismal (no capitals) <118924562+Dismalitie@users.noreply.github.com>
2023-11-27 18:18:34 +0000

    bugfix 3 😭😭

--- JaxCore/uninstall.core.bat
+++ JaxCore/uninstall.core.bat
@@ -2,5 +2,6 @@
 echo JaxCore-CS Uninstaller v1.1
 del Siticone.UI.dll && echo [Siticone]:[UI]:[dll] Removed
 del JaxCore.exe && echo [JaxCore]:[CS]:[exe] Removed
-del .uninstall.bat  && echo [core]:[uninstall]:[bat] Removed
+del .uninstall.bat && echo [core]:[uninstall]:[bat] Removed
+del installer.core.bat && echo [core]:[installer]:[bat] Removed
 echo You can now close this window and remove this file.
