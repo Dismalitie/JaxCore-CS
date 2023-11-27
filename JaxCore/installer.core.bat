741ba140008f6d9df6d3816e080cc63e2cc91883
dismal (no capitals) <118924562+Dismalitie@users.noreply.github.com>
2023-11-27 17:44:08 +0000

    Update installer.core.bat

--- JaxCore/installer.core.bat
+++ JaxCore/installer.core.bat
@@ -13,7 +13,7 @@ echo [installer]:[cs-launcher]:[setup] Locating directory...
 echo %CD% > launcher.path && echo [installer]:[cs-launcher]:[setup] Located directory.
 
 echo [installer]:[cs-launcher]:[exe] Installing executable...
-powershell.exe curl https://github.com/Dismalitie/JaxCore-CS/raw/main/JaxCore/bin/Debug/JaxCore.exe -O JaxCore.exe && echo [installer]:[cs-core]:[exe] Executable installed.
+powershell.exe curl https://github.com/Dismalitie/JaxCore-CS/raw/main/JaxCore-Launcher/bin/Debug/JaxCore-CS.exe -O JaxCore-CS.exe && echo [installer]:[cs-launcher]:[exe] Executable installed.
 
 
-start JaxCore.exe
+start JaxCore-CS.exe
