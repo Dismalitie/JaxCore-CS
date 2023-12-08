b7a89783202b3016d35a74b7c8c4c7576ec7d31e
dismal (no capitals) <118924562+Dismalitie@users.noreply.github.com>
2023-12-08 12:17:32 +0000

    Add graphite indexes

--- JaxCore-Installer/installer.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd %1%
-
-powershell.exe curl https://github.com/Dismalitie/JaxCore-CS/raw/main/JaxCore/bin/Debug/Siticone.UI.dll -O Siticone.UI.dll
-powershell.exe curl https://github.com/Dismalitie/JaxCore-CS/raw/main/JaxCore/bin/Debug/JaxCore.exe -O JaxCore.exe
